80c942b0a2c04dc79545b068cad6b33f1774787a
Lee Hoseop <ghtjq3909@naver.com>
2022-01-07 22:15:28 +0900

    initial commit

---
+++ src/main/java/com/example/nnk/NnkApplication.java
@@ -0,0 +1,13 @@
+package com.example.nnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnkApplication.class, args);
+	}
+
+}
